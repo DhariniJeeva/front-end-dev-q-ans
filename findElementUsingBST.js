88a66d2071cfb956a3a50c26c6cb8fd29bfefecf
DJ <dharinij.it@gmail.com>
2018-07-18 12:08:15 -0400

    fixed spaces

--- findElementUsingBST.js
+++ findElementUsingBST.js
@@ -23,15 +23,11 @@ function findEle(arr,val){
     else {
     	high  = mid -1 ;
     }
-   
-    
   }
-
    return -1;
 }
 const arr = [1,2,3,4,5,6,7,8,9];
 const val = 12;
-
 console.log(findEle(arr, val));
 
 //output is -1 cos 12 is not in the list!
